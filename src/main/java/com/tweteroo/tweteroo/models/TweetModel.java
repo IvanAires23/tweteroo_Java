55d772a9512126181b949f5e4a72eef06973d768
IvanAires23 <ivanaires32@gmail.com>
2024-07-29 18:15:37 -0300

    feat: column join

--- src/main/java/com/tweteroo/tweteroo/models/TweetModel.java
+++ src/main/java/com/tweteroo/tweteroo/models/TweetModel.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,18 +22,18 @@ import lombok.NoArgsConstructor;
 public class TweetModel {
 
     public TweetModel(TweetDTO dto){
-        this.userId = dto.getUserId();
         this.text = dto.getText();
     }
 
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     @Column(nullable = false)
-    private Long userId;
-
-    @Column(nullable = false)
     private String text;
+
+    @ManyToOne
+    @JoinColumn(name = "authorId")
+    private UserModel author;
 }
